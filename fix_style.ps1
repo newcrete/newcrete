a960cf789444e6f81521031011b1caa39efb47fe
JEONGYEAHUN <jeongyeahun@users.noreply.github.com>
2026-02-06 17:49:24 +0900

    Increase spacing between checkmark and text and update to v60

--- fix_style.ps1
+++
@@ -1,7 +0,0 @@
-$path = "style.css"
-$c = Get-Content $path -Encoding Default
-# Line 693 corresponds to index 692 (0-indexed)
-$c[692] = '    content: "\2713";'
-# Line 713 corresponds to index 712 (0-indexed)
-$c[712] = '    content: "\2713";'
-$c | Out-File $path -Encoding UTF8
